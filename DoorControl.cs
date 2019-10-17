1b31ce2ecd19b6d317aeba9ac1b046630b08d4c3
Richard <44157733+BanksR@users.noreply.github.com>
2019-10-17 13:57:03 +0100

    Add files via upload
    Init commit

---
+++ DoorControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    private Animator _anims;
+    public Collider _doorCollider;
+
+    public int DoorID = 0;
+
+    private void Awake()
+    {
+        _anims = GetComponent<Animator>();
+    }
+
+
+    // Upon a trigger, if zombie let it pass.
+    // If Player do the OpenDoor Function.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zombie"))
+        {
+            _anims.SetTrigger("Open");
+            _doorCollider.enabled = false;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            KeyChain keysInInventory = other.GetComponent<KeyChain>();
+            OpenDoor(keysInInventory);
+        }
+    }
+
+    
+    // This function will loop through every key in the players
+    // KeyChain List of Keys. If it finds a key that matches the DoorID,
+    // it will open the door.
+    public void OpenDoor(KeyChain keysToCheck)
+    {
+        foreach (var key in keysToCheck.keys)
+        {
+            if (key.keyID == DoorID)
+            {
+                _anims.SetTrigger("Open");
+                keysToCheck.RemoveKey(key);
+                _doorCollider.enabled = false;
+                return;
+            }
+        }
+        
+        Debug.Log("Door Remains Locked - find the correct Key!");
+    }
+}
---
+++ Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public int keyID;
+    public string keyName;
+    private KeyChain keyChain;
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.gameObject.CompareTag("Player"))
+        {
+
+	        keyChain = other.GetComponent<KeyChain>();
+            // Add this Key to our KeyChain
+            // Play a pick-up SFX
+            keyChain.AddKey(this);
+	        Debug.Log("You picked up the "+keyName+ ". and add it to your Keychain");
+
+            this.gameObject.SetActive(false);
+        }
+	}
+}
